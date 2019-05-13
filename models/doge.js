7e39ab1711c793ba762ff9ec6dd2ccfee54eba8d
jepidoptera <jepidoptera@live.com>
2019-05-12 22:06:55 -0500

    works now

--- models/doge.js
+++ models/doge.js
@@ -1,35 +1,41 @@
 module.exports = function(sequelize, DataTypes) {
     var Doges = sequelize.define("Doges", {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        breed: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        description: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        location: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        birthdate: {
-            type: DataTypes.DATE
-        },
-        visible: {
-            type: DataTypes.BOOLEAN
-        },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      breed: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      location: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      birthdate: {
+        type: DataTypes.DATE
+      },
+      sex: {
+        type: DataTypes.STRING
+      },
+      imgURL: {
+        type: DataTypes.STRING
+      },
+      visible: {
+        type: DataTypes.BOOLEAN
+      }
     });
     return Doges;
 };
